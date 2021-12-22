d1f077c007297406f90910507afa75e4fe53e058
pronoy2108 <83197609+pronoy2108@users.noreply.github.com>
2021-12-22 10:24:15 +0530

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,42 +1,56 @@
 <script lang="ts">
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
-	import * as Fl from "fluent-svelte";
+	import * as Fluent from "fluent-svelte";
+	let dialogOpen = false;
 </script>
 
 <Header />
 
 <main>
-	<slot />
+	<slot/>
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	
 </footer>
 
 <style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+	@media(prefers-color-scheme: light){
+		:root{
+			font-size: 16px;
+			text-align: left;
+			color: black;
+		}
+	}
+	@media(prefers-color-scheme: dark){
+		:root{
+			font-size: 16px;
+			text-align: left;
+			color: white;
+		}
+	}
 	main {
 		flex: 1;
 		display: flex;
 		flex-direction: column;
 		padding: 1rem;
 		width: 100%;
-		max-width: 1024px;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-
+	
 	footer {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+		justify-content: stretch;
+		align-items: stretch;
 		padding: 40px;
 	}
-
 	@media (min-width: 480px) {
 		footer {
-			padding: 40px 0;
+			padding: 0px 40px 40px;
 		}
 	}
 </style>
