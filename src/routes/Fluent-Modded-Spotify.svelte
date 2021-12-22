502a7ffbe534df6086ebd043ed967cc55b756a1d
pronoy2108 <83197609+pronoy2108@users.noreply.github.com>
2021-12-22 15:27:29 +0530

    Created Fluent-Modded-Spotify.svelte at routes

---
+++ src/routes/Fluent-Modded-Spotify.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Header from "$lib/header/Header.svelte";
+	import * as Fluent from "fluent-svelte";
+	import "fluent-svelte/theme.css";
+	let dialogOpen = false;
+</script>
+<h2 align="center">
+  
+  Fluent-Modded-Spotify
+
+  
+  <img align="center" width="256" height="256" src="https://raw.githubusercontent.com/pronoy2108/Fluent-Modded-Spotify/main/spotify-icon.png">
+  </h2>
+  
+  <p align="center">Icon made by <a href="https://t.me/Qirkl">Qirkl</a></p>
+<p align="center">Spotify, but with a hint of Fluent Design</p>
+  
+
+<p align="center">
+<a href="https://github.com/pronoy2108/Fluent-Modded-Spotify/releases/tag/v2.5"><img src="https://img.shields.io/github/v/release/pronoy2108/Fluent-Modded-Spotify?label=version"></a>
+
+<img src="https://img.shields.io/github/downloads/pronoy2108/Fluent-Modded-Spotify/total">
+</p>
+<hr/>
+
+</Fluent.ContentDialog>
+</div>
+<div class="pad">
+	<Fluent.Expander>Credits
+	<svelte:fragment slot="content">
+<h3><a href="https://github.com/williamckha/spicetify-fluent">williamckha/spicetify-fluent</a></h3>
+		<Fluent.TextBlock></Fluent.TextBlock>
+		<Fluent.TextBlock>
+			<a href="https://github.com/khanhas/spicetify-cli">Spicetify</a> theme inspired by Windows 11 UI and Microsoft's Fluent Design
+            </div>
+
+<br>
+
+<img src="https://github.com/williamckha/spicetify-fluent/blob/master/screenshots/dark-1.png?raw=true" alt="dark-1">
+<br>
+<img src="https://github.com/williamckha/spicetify-fluent/blob/master/screenshots/dark-2.png?raw=true" alt="dark-2">
+<br>
+<img src="https://github.com/williamckha/spicetify-fluent/blob/master/screenshots/light-1.png?raw=true" alt="light-1">
+<br>
+
+<Fluent.Expander>
+    <h2>About</h2>
+</Fluent.Expander>
+
+		</Fluent.TextBlock>
+	</svelte:fragment>
+</Fluent.Expander>
+
