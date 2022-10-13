c10ce2e8a7df5bfdfab1c313b3e9c91b91e3f7b0
Pronoy Mukherjee <83197609+pronoy2108@users.noreply.github.com>
2022-10-13 16:06:56 +0530

    ddddd

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,11 +4,14 @@
 	import "fluent-svelte/theme.css";
 	let dialogOpen = true;
 </script>
-<h1 class="ctext"><p>Fluent Search</p></h1>
+
+<h1 align="center"><p>Fluent Search</p></h1>
 <TextBlock type="search" placeholder="Search the web" form method="get" action="https://www.google.com/search"></TextBlock>
 <div class="pad ctext">
 <Fluent.Button variant="standard" href="https://www.google.com/doodles" target="_blank" rel="noopener noreferrer">I'm Feeling Lucky</Fluent.Button>
 </div>
+
+
 <style>
 	@import url("https://unpkg.com/fluent-svelte/theme.css");
 	@media(prefers-color-scheme: light){
