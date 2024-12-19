26942fc33d450d654f501a46b511d6baace85458
Pac. <83197609+pacman2108@users.noreply.github.com>
2024-12-20 02:39:31 +0530

    test 2

--- src/routes/Fluentsearch/index.svelte
+++ src/routes/Fluentsearch/index.svelte
@@ -2,17 +2,18 @@
 	import Header from "$lib/header/Header.svelte";
 	import * as Fluent from "fluent-svelte";
 	import "fluent-svelte/theme.css";
-	let dialogOpen = true; // Consider implementing dialog logic if needed
+	let dialogOpen = true;
 </script>
 
-<h1 align="center">Fluent Search</h1>
-<input type="text" placeholder="Search the web" form method="get" action="https://www.google.com/search" />
-
+<h1 align="center"><p>Fluent Search</p></h1>
+<TextBlock type="search" placeholder="Search the web" form method="get" action="https://www.google.com/search"></TextBlock>
 <div class="pad ctext">
-	<button class="fluent-button" onclick="window.open('https://www.google.com/doodles', '_blank')">I'm Feeling Lucky</button>
+<Fluent.Button variant="standard" href="https://www.google.com/doodles" target="_blank" rel="noopener noreferrer">I'm Feeling Lucky</Fluent.Button>
 </div>
 
+
 <style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
 	@media(prefers-color-scheme: light){
 		:root{
 			font-size: 16px;
