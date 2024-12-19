5ef6ed8b4b99c66ab7bbb78686c563f8bed82cc3
Pac. <83197609+pacman2108@users.noreply.github.com>
2024-12-20 03:11:31 +0530

    ..

--- src/routes/Fluentsearch/index.svelte
+++ src/routes/Fluentsearch/index.svelte
@@ -2,20 +2,13 @@
 	import Header from "$lib/header/Header.svelte";
 	import * as Fluent from "fluent-svelte";
 	import "fluent-svelte/theme.css";
-	let dialogOpen = true;
-	let query = '';
-	
-	function handleSubmit(event) {
-		event.preventDefault();
-		const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-		window.open(searchUrl, '_blank');
-	}
+	import { TextBox } from "fluent-svelte";
 </script>
 
 <h1 align="center">Fluent Search</h1>
 <form on:submit={handleSubmit}>
 	<!--<input type="search" bind:value={query} placeholder="Search the web" />-->
-<TextBox placeholder="Search the web" type="search" bind:value={query} style="text-align:center"/>
+<TextBox type="search" placeholder="Search the web" style="text-align:center"/>
 	<div class="pad ctext">
 		<Fluent.Button variant="standard" type="submit">Search</Fluent.Button>
 	</div>
