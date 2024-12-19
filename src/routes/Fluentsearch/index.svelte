52d834d435077af1cc5f39fa461dc06765ed9487
Pac. <83197609+pacman2108@users.noreply.github.com>
2024-12-20 01:05:37 +0530

    Bugfixes for Fluentsearch + other fixes

--- src/routes/Fluentsearch/index.svelte
+++ src/routes/Fluentsearch/index.svelte
@@ -2,15 +2,22 @@
 	import Header from "$lib/header/Header.svelte";
 	import * as Fluent from "fluent-svelte";
 	import "fluent-svelte/theme.css";
-	let dialogOpen = true;
+	let query = '';
+	
+	function handleSubmit(event) {
+		event.preventDefault();
+		const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+		window.open(searchUrl, '_blank');
+	}
 </script>
 
-<h1 align="center"><p>Fluent Search</p></h1>
-<TextBlock type="search" placeholder="Search the web" form method="get" action="https://www.google.com/search"></TextBlock>
-<div class="pad ctext">
-<Fluent.Button variant="standard" href="https://www.google.com/doodles" target="_blank" rel="noopener noreferrer">I'm Feeling Lucky</Fluent.Button>
-</div>
-
+<h1 align="center">Fluent Search</h1>
+<form on:submit={handleSubmit}>
+	<Fluent.TextBlock type="search" bind:value={query} placeholder="Search the web"></Fluent.TextBlock>
+	<div class="pad ctext">
+		<Fluent.Button variant="standard" type="submit">Search</Fluent.Button>
+	</div>
+</form>
 
 <style>
 	@import url("https://unpkg.com/fluent-svelte/theme.css");
